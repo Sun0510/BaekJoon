c3ce1ef49c406e8ca41de27f4ec954cb45c8f488
Sun0510 <99360766+Sun0510@users.noreply.github.com>
2023-12-21 18:43:59 +0900

    Create 2293.c
    DP Algorithm / 동전 1

---
+++ 2293.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int n, k, cnt = 0;
+	int temp;
+	scanf("%d %d", &n, &k);
+
+	int *arr = (int*)malloc(sizeof(int) * (k + 1));
+	for (int i = 0; i < k + 1; i++) {
+		arr[i] = 0;
+	}
+	
+	int* c = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &temp);
+		c[i] = temp;
+	}
+
+	arr[0] = 1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = c[i]; j < k + 1; j++) {
+			arr[j] += arr[j - c[i]];
+		}
+	}
+
+	printf("%d\n", arr[k]);
+	free(arr);
+	free(c);
+
+	return 0;
+}
