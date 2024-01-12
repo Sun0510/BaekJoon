83b06ce52143db605d27bdcb051391f3584f9cc2
순형 <99360766+Sun0510@users.noreply.github.com>
2024-01-12 23:56:16 +0900

    DP
    다이나믹 프로그래밍 / 암호코드

--- 2011.c
+++ 2011.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MOD 1000000
+
 int dp[5001];
 char code[5001];
 
 int fun(int n, int size) {
     if (n >= size)
         return 1;
-    int ret = dp[n];
 
+    int ret = dp[n];
     if (ret != -1) 
         return ret;
+
     if (code[n] == '0')
         return 0;
 
     ret = fun(n + 1, size);
-    if (n + 1 < size) {
-        if (code[n] <= '2' && code[n+1] <= '6') {
-            ret += fun(n + 2, size);
-        }
+    if (n + 1 < size && (code[n] == '1' || (code[n] == '2' && code[n + 1] <= '6'))) {
+        ret = (ret + fun(n + 2, size)) % MOD;
     }
-    return (ret % 1000000);
+
+    dp[n] = ret;
+    return ret;
 }
+
 int main() {
-    scanf("%s", &code);
+    scanf("%s", code);
     memset(dp, -1, sizeof(dp));
 
     int size = strlen(code);
-
     printf("%d\n", fun(0, size));
 
     return 0;
