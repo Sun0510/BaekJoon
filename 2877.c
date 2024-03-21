7bf6bfd968492387e6ae53af2b4877a194a0a2ea
순형 <99360766+Sun0510@users.noreply.github.com>
2024-03-22 00:13:27 +0900

    Math
    수학, 구현 / 4와 7

--- 2877.c
+++ 2877.c
@@ -1,1 +1,26 @@
-asd
+#include <stdio.h>
+
+int arr[1000000];
+
+int main() {
+	int K, remainder, num = 0;
+	scanf("%d", &K);
+
+	K++;
+
+	while (K >= 1) {
+		remainder = K % 2;
+		K /= 2;
+		if (remainder == 0) 
+			arr[num++] = 4;
+		else
+			arr[num++] = 7;
+	}
+
+	for (int i = num - 2; i >= 0; i--) {
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
